507d76f0e635e38782ff4f696e149497687582b2
soken <nsongkien@gmail.com>
2024-02-29 16:27:01 +0200

    ALL DONEEEE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css';
-import {inter} from '@/app/ui/fonts';
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  //Title template at Root
+  title: {
+    template: '%s | Acme Dashboard', //template
+    default: 'Acme Dashboard',
+  },
+  description: 'a very long and detailed desc',
+  metadataBase: new URL('https://nskien.xyz'),
+};
 
 export default function RootLayout({
   children,
